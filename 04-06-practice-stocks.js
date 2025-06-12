96a28e0d8fc03a2af5eac164e6f5567c99c1fee3
stolyarjane <stolyarjane01@gmail.com>
2025-06-12 17:06:48 +0400

    12-06-practice

--- 04-06-practice-stocks.js
+++ 04-06-practice-stocks.js
@@ -26,28 +26,59 @@ function createArray(day1, day2) {
 function maxProfit(prices) {
     let minPrice = prices[0];
     let maxProfit = 0;
-    let buyDay = 0;
-    let sellDay = 0;
+    let minDay = 0; // день покупки по минимальной цене
+    let buyDay = -1; // день покупки (результат)
+    let sellDay = -1; // день продажи (результат)
     for (let i = 1; i < prices.length; i++){
-        const currentPrice = prices[i];
+        console.log(`i = ${i}`);
+        console.log(`minPrice = ${minPrice}; maxProfit = ${maxProfit}; minDay = ${minDay}; buyDay = ${buyDay}; sellDay = ${sellDay}`);
+
+        const currentPrice = prices[i]; 
+        console.log(`const currentPrice = prices[${i}]; // ${prices[i]}`);
+        
         // пробуем вычислить потенциальную прибыль, если бы купили по минимальной цене до дня №i и продали по текущей цене
         const potentialProfit = currentPrice - minPrice;
+        console.log(`const potentialProfit = currentPrice(${currentPrice}) - minPrice(${minPrice}); // potentialProfit = ${potentialProfit}`);
+
+        console.log(`potentialProfit(${potentialProfit}) > maxProfit(${maxProfit}) ? `)
 
         if (potentialProfit > maxProfit){
+            console.log(`да, больше`);
+            console.log(`maxProfit было: ${maxProfit}; buyDay было: ${buyDay}; minDay было: ${minDay}`);
             maxProfit = potentialProfit;
-            buyDay = 
+            console.log(`potentialProfit присвоили к maxProfit. maxProfit стал равен: ${maxProfit}`);
+            buyDay = minDay;
+            console.log(`buyDay присвоили к minDay. buyDay стал равен: ${buyDay}`);
+            sellDay = i;
+            console.log(`sellDay: ${sellDay}`);
+        } else {
+            console.log(`нет, меньше`);
         }
+        console.log(`--- вышли из блока IF ---`);
+        
+
+        console.log(`currentPrice(${currentPrice}) < minPrice(${minPrice}) ? `);
         if (currentPrice < minPrice){
+            console.log(`да, меньше`);
+            console.log(`minPrice было: ${minPrice}; currentPrice было: ${currentPrice}; minDay было: ${minDay}`);
             minPrice = currentPrice;
+            console.log(`currentPrice присвоили к minPrice. minPrice стал равен: ${minPrice}`);
+            minDay = i; 
+            console.log(`minDay: ${minDay}`);
+        } else{
+            console.log(`нет, больше`);
         }
+        console.log(`--- вышли из блока IF ---`);
+        console.log(`--- конец итерации №${i} -----------`);
+        console.log();
     }
-    return (maxProfit > 0) ? `максимальная прибыль составляет ${maxProfit}. покупка в день № по цене ` : "невозможно получить прибыль" // тернарный оператор 
+    return (maxProfit > 0) ? `максимальная прибыль составляет ${maxProfit}. покупка в день №${buyDay} по цене ${minPrice}, продажа в день №${sellDay} по цене ${prices[sellDay]}` : "невозможно получить прибыль" // тернарный оператор 
     // если maxProfit > 0, то return maxProfit; 
     // иначе return "невозможно получить прибыль"
     
 }
 
-let createdArray = createArray(1, 30);
+let createdArray = createArray(1, 5);
 let calculatedMaxProfit = maxProfit(createdArray);
 
 console.log(createdArray);
